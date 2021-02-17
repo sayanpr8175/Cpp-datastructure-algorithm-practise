41d704fa19dc3228341c50f72dfa0981e12cb7c7
Sayan <sayan.pramanik8175@gmail.com>
2021-02-17 18:56:32 +0530

    Recursion problems /remove duplicates file created

---
+++ Recursion problems/Removeduplicates.cpp
@@ -0,0 +1,18 @@
+
+/*
+	** Remove Duplicates Recursively ** 
+	Given a string S, remove consecutive duplicates from it recursively.
+	
+	Sample Input 1 : 	aabccba
+	Sample Output 1 :   abcba
+	    
+	Sample Input 2 :    xxxyyyzwwzzz
+	Sample Output 2 :   xyzwz
+		
+*/
+
+#include<iostream>
+using namespace std;
+
+
+
