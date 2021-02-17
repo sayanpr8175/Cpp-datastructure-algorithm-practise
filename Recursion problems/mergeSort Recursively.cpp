e090cb855c51e4a27259d147be851a0c76ece699
Sayan <sayan.pramanik8175@gmail.com>
2021-02-17 22:55:41 +0530

    mergesort finalised

--- Recursion problems/mergeSort Recursively.cpp
+++ Recursion problems/mergeSort Recursively.cpp
@@ -1,3 +1,25 @@
+/*
+	Sort an array A using Merge Sort.
+	Input format :
+	
+	Line 1 : Integer n i.e. Array size
+	Line 2 : Array elements (separated by space)
+	
+	Output format :
+	Array elements in increasing order (separated by space)
+	
+	Sample Input 1 :
+	> 6 
+	> 2 6 8 5 4 3
+    Sample Output 1 :  2 3 4 5 6 8
+
+	Sample Input 2 :
+	> 5
+	> 2 1 5 2 3
+	Sample Output 2 :  1 2 2 3 5 
+*/
+
+
 #include <iostream>
 using namespace std;
 
