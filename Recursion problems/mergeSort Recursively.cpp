b22a8270a75c1b2639cc4a6aa40217ceb0f1fa81
Sayan <sayan.pramanik8175@gmail.com>
2021-02-17 20:47:58 +0530

    Recursion problems /mergeSort recursively created

---
+++ Recursion problems/mergeSort Recursively.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+void merge(int a[], int startIndex, int endIndex){
+	int arr[10000];
+	int mid = (startIndex+endIndex)/2;
+	int i = startIndex;
+	int j = mid+1;
+	int k = startIndex;
+	
+	while(i<=mid && j<=endIndex){
+		if(a[i]<a[j]){
+			arr[k] = a[i];
+			k++; i++;
+		}
+		
+		else if(a[j]<a[i]){
+			
+			arr[k] = a[j];
+			k++; j++;
+		}
+		
+		else{
+			arr[k] = a[i]; k++; i++;
+			arr[k] = a[j]; k++; j++;
+		}
+		
+		while(i<=mid){
+			arr[k] = a[i];
+			k++; i++;
+		}
+		
+		while(j<=endIndex){
+			arr[k] = a[j];
+			k++; j++;
+		}
+		
+		for(int i=startIndex; i<=endIndex; i++){
+			a[i] = arr[i];
+		}
+		
+	}
+	
+	
+	
+}
+
+void mergeSort(int a[], int startIndex, int endIndex){
+	// base case
+	if(startIndex>=endIndex){
+		return;
+	}
+	
+	int mid = (startIndex+endIndex)/2;
+	
+	mergeSort(a, startIndex, mid);
+	mergeSort(a, mid+1, endIndex);
+	
+	merge(a, startIndex, endIndex);
+	
+}
+
+void mergeSortPass(int a[], int size){
+	// nothing to sort case
+	if(size==0 || size==1){
+		return;
+	}
+	
+	mergeSort(a, 0, size);
+}
+
+int main(){
+	
+}
