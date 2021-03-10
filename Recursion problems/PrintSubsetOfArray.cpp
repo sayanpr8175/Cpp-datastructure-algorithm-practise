50a6c9f9f6b39484792df964e19ce46f4ad8a298
Sayan <sayan.pramanik8175@gmail.com>
2021-03-10 18:14:15 +0530

    Recursion problems /PrintSubsetOfArray.cpp added

---
+++ Recursion problems/PrintSubsetOfArray.cpp
@@ -0,0 +1,29 @@
+/*
+	 Print Subsets of Array 
+	 Given an integer array (of length n), find and print all the subsets of input array.
+	 
+	 Subsets are of length varying from 0 to n, that contain elements of the array.
+	 But the order of elements should remain same as in the input array.
+	 Note : The order of subsets are not important. Just print the subsets in different lines.
+	
+	Sample Input:
+				3
+				15 20 12
+	
+	Sample Output:
+				12 
+				20 
+				20 12 
+				15 
+				15 12 
+				15 20 
+				15 20 12 
+	
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+}
